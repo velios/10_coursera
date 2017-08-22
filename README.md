b9deeb959b449b0d528c83a329b51ea4b64ce490
velios <prostozapomnit@gmail.com>
2017-08-23 00:00:53 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,7 +1,25 @@
 # Coursera Dump
 
-[TODO. There will be project description]
+The program collects data from N courses in [Coursera](https://www.coursera.org) and saves the received data to a file
+
+### How to Install
+
+Python 3 should be already installed. Then use pip (or pip3 if there is a conflict with old Python 2 setup) to install dependencies:
+
+```bash
+pip install -r requirements.txt # alternatively try pip3
+```
+
+### How to use
+```bash
+#Run
+$ python3 coursera.py
+#Sample output
+The program recorded data from 20 random courses on Coursera.org into file filename.xlsx
+```
+Remember, it is recommended to use [virtualenv/venv](https://devman.org/encyclopedia/pip/pip_virtualenv/) for better isolation.
 
 # Project Goals
 
+This README made with [Dilinger](http://dillinger.io/)
 The code is written for educational purposes. Training course for web-developers - [DEVMAN.org](https://devman.org)
--- coursera.py
+++ coursera.py
@@ -1,64 +1,69 @@
 from lxml import etree
 from bs4 import BeautifulSoup
+from openpyxl import Workbook
+from collections import OrderedDict
 import re
 import requests
+import random
 
 
-
-def get_coursera_courses_list():
-    courses_text_response = requests.get('https://www.coursera.org/sitemap~www~courses.xml').text
+def get_coursera_courses_list(quantity):
+    coursera_sitemap_url = 'https://www.coursera.org/sitemap~www~courses.xml'
+    courses_text_response = requests.get(coursera_sitemap_url).text
     courses_xml_data = etree.fromstring(courses_text_response.encode('utf-8'))
-    courses_list = []
-    for url_tag_data in courses_xml_data.getchildren():
-        for loc_tag_data in url_tag_data.getchildren():
-            courses_list.append(loc_tag_data.text)
-    return courses_list[:20]
+    courses_list = [loc_tag_data.text
+                    for url_tag_data in courses_xml_data.getchildren()
+                    for loc_tag_data in url_tag_data.getchildren()]
+    return random.sample(courses_list, quantity)
 
 
-# Зайти на страницу курса и вытащить оттуда
-# название, язык, ближайшую дату начала, количество недель и среднюю оценку
 def get_course_info(course_url):
-    parse_results = []
     course_text_response = requests.get(course_url).content
-    course_bs_parser = BeautifulSoup(course_text_response, 'html.parser')
-    coursera_course_name = course_bs_parser.find('h1', {'class': 'title'}).text
-    coursera_course_language = re.findall('^[a-zA-Z]+', course_bs_parser.find('div', {'class': 'rc-Language'}).text)[0]
+    course_parser = BeautifulSoup(course_text_response, 'html.parser')
+    coursera_course_name = course_parser.find('h1', {'class': 'title'}).text
+    coursera_course_language = re.findall('^[a-zA-Z]+', course_parser.find('div', {'class': 'rc-Language'}).text)[0]
     try:
-        coursera_course_rating = re.findall('[\d.]+', course_bs_parser.find('div', {'class': 'ratings-text'}).text)[0]
+        coursera_course_rating = re.findall('[\d.]+', course_parser.find('div', {'class': 'ratings-text'}).text)[0]
     except AttributeError:
         coursera_course_rating = ''
     try:
-        coursera_course_start_date = course_bs_parser.find('div', {'class': 'rc-StartDateString'}).text        
+        coursera_course_start_date = course_parser.find('div', {'class': 'rc-StartDateString'}).text
     except:
         coursera_course_start_date = ''
     try:
-        coursera_course_duration = course_bs_parser.find('td', {'class': 'td-data'}).text
+        coursera_course_duration = course_parser.find('td', {'class': 'td-data'}).text
     except:
         coursera_course_duration = ''
-    parse_results.append({
-            'course_name': coursera_course_name,
-            'course_language': coursera_course_language,
-            'course_rating': coursera_course_rating,
-            'course_start_date': coursera_course_start_date,
-            'course_duration': coursera_course_duration
-        })
+    parse_results = OrderedDict([
+        ('course_name', coursera_course_name),
+        ('course_language', coursera_course_language),
+        ('course_rating', coursera_course_rating),
+        ('course_start_date', coursera_course_start_date),
+        ('course_duration', coursera_course_duration),
+        ('courrse_url', course_url)
+    ])
     return parse_results
 
 
-def output_courses_info_to_xlsx(filepath):
-    pass
+def output_courses_info_to_xlsx(course_data_dicts_list, output_filepath):
+    workbook = Workbook(write_only=True)
+    worksheet = workbook.create_sheet()
+    is_write_title_row = True
+    for course_data in course_data_dicts_list:
+        if is_write_title_row:
+            worksheet.append(list(course_data.keys()))
+            is_write_title_row = False
+        worksheet.append(list(course_data.values()))
+    try:
+        workbook.save(output_filepath)
+    except PermissionError:
+        print('The program can not write a file with courses')
 
 
 if __name__ == '__main__':
-    # course_info = requests.get('https://www.coursera.org/learn/protools')
-    # soup = BeautifulSoup(course_info.text, "html.parser")
-    # coursera_xml_data = requests.get('{}/sitemap~www~courses.xml'.format(COURSERA_URL)).text
-    # xml_data = etree.parse(coursera_xml_data)
-    # print(etree.fromstring(coursera_xml_data))
-    # xml_data = etree.fromstring(coursera_xml_data.encode('utf-8'))
-    # print(xml_data.find('loc').tag)
-    # parseXML(coursera_xml_data)
-    # coursera_course_list = get_coursera_courses_list()
-    courses_list = get_coursera_courses_list()
-    course_info = [ get_course_info(course) for course in courses_list ]
-
+    course_quantity = 5
+    result_file_name = 'result_file.xlsx'
+    coursera_courses_url_list = get_coursera_courses_list(course_quantity)
+    list_with_courses_data = [get_course_info(course_url) for course_url in coursera_courses_url_list]
+    output_courses_info_to_xlsx(list_with_courses_data, result_file_name)
+    print('The program recorded data from {} random courses on Coursera.org into file {}'.format(course_quantity, result_file_name))
